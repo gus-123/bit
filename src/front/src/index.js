537c2ca60e9949361a63c31465e487100842c850
gus-123 <hyunmin1711@gmail.com>
2023-12-22 15:42:43 +0900

    Front-End & Back-End

--- src/front/src/index.js
+++ src/front/src/index.js
@@ -1,40 +1,40 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import './index.css';
-// //import 'bootstrap/dist/css/bootstrap.css';
-// import App from './App';
-// import reportWebVitals from './reportWebVitals';
-// import axios from 'axios';
-//
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// axios.defaults.baseURL = "http://localhost:8080/bit"
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-//
-// // If you want to start measuring performance in your app, pass a function
-// // to log results (for example: reportWebVitals(console.log))
-// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
-
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 //import 'bootstrap/dist/css/bootstrap.css';
-import LoginPage from './LoginPage'; // Import LoginPage
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 import axios from 'axios';
 
-axios.defaults.baseURL = "http://localhost:8080/bit";
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
+axios.defaults.baseURL = "http://localhost:8080/bit"
 root.render(
-    <React.StrictMode>
-        <LoginPage /> {/* Render LoginPage directly */}
-    </React.StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// //import 'bootstrap/dist/css/bootstrap.css';
+// import LoginPage from './LoginPage'; // Import LoginPage
+// import reportWebVitals from './reportWebVitals';
+// import axios from 'axios';
+//
+// axios.defaults.baseURL = "http://localhost:8080/bit";
+//
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//     <React.StrictMode>
+//         <LoginPage /> {/* Render LoginPage directly */}
+//     </React.StrictMode>
+// );
+//
+// reportWebVitals();
